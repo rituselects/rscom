a1dc85d7c76c3fa07ec8835ad67c218c3e2712fe
Pravin Bhat <bhatpravin@gmail.com>
2019-06-08 19:39:52 -0400

    Lookbook updates

--- js/lookbook.js
+++ js/lookbook.js
@@ -189,3 +189,167 @@ $("#carousel5").owlCarousel({
         }
     }
 });
+
+
+$("#carousel6").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
+
+
+$("#carousel7").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
+
+
+$("#carousel8").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
+
+
+$("#carousel9").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
+
+$("#carousel10").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
