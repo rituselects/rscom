6738df12ebc6dc2a127e313ff0923cb171187f71
aenosys <aenosys@gmail.com>
2019-06-12 09:55:11 +0530

    email id

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('nsalma006@gmail.com'); // ← Your email here
+$pp->sendEmailTo('info@rituselects.com'); // ← Your email here
 
 echo $pp->process($_POST);
